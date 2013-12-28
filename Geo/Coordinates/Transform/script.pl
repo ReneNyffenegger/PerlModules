012f75d6d1311b4c9733215c5112ea870c773fa3
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2013-12-28 18:41:52 +0100

    + Geo::Coordinates::Transform

---
+++ Geo/Coordinates/Transform/script.pl
@@ -0,0 +1,12 @@
+use warnings; use strict;
+use Geo::Coordinates::Transform;
+
+
+my $g = new Geo::Coordinates::Transform();
+
+
+printf "%s\n", @{$g->cnv_to_dms([ 20.5     ])}[0];   # 20 30 0.00000
+printf "%s\n", @{$g->cnv_to_ddm([ 20.5     ])}[0];   # 20 30.00000
+printf "%s\n", @{$g->cnv_to_dd (["20 30 0" ])}[0];   # 20.5000000
+printf "%s\n", @{$g->cnv_to_dd (["20 30 12"])}[0];   # 20.5033333
+
