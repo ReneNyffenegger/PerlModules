36406e910b90c68d3986fab1fe056d6efad982af
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-01-10 15:09:22 +0100

    + ttf.pl

---
+++ GD/ttf.pl
@@ -0,0 +1,25 @@
+#
+#   See also http://www.adp-gmbh.ch/perl/gd/index.html
+#
+use warnings;
+use strict;
+
+use GD;
+
+#  Use forward slashes!
+my $ttf_path='/todo/fonts/OpenSans/OpenSans-Regular.ttf';
+
+my $image = new GD::Image(400, 400);
+my $grey  = $image->colorAllocate(100, 100, 100);
+
+# Background:
+$image->fill(0, 0, $grey);
+
+my $red   = $image->colorAllocate(255,  0,  0);      
+
+$image->stringFT($red, $ttf_path, 20, 3.141/2, 200, 300, "Here's the text.");
+
+open  my $o, '>', "c:/temp/gd_ttf.png";
+binmode  $o;
+print    $o $image->png;
+close    $o;
