fde563e556c834ebc958fa3947e5bbefee3a0d13
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-03-13 14:06:23 +0100

    Clarification

--- Time/Piece/script.pl
+++ Time/Piece/script.pl
@@ -8,6 +8,6 @@ my $t1 = Time::Piece -> strptime ('01:50', '%H:%M');
 my $t2 = Time::Piece -> strptime ('03:05', '%H:%M');
 
 
-my $t_ = $t2 - $t1;
+my $t_ = $t2 - $t1;  # The result is a Time::Seconds object.
 
 say "Hours between: ", $t_ -> hours;
