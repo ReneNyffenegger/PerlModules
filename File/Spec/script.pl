c9d711584ebe5808cf25ef080e8996d52fde2e46
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-04-14 06:58:22 +0200

    + File::Spec

---
+++ File/Spec/script.pl
@@ -0,0 +1,55 @@
+use warnings; use strict;
+
+use File::Spec;
+
+use Cwd;
+
+
+print "canonpath\n";
+print "---------\n";
+
+print "\n" .                       cwd ;
+print "\n" . File::Spec->canonpath(cwd);
+print "\n\n"                           ;
+
+print "catdir\n";
+print "------\n";
+
+print "\n" . File::Spec->catdir('..', '..');
+print "\n\n"                           ;
+
+print "catfile\n";
+print "------\n";
+
+print "\n" . File::Spec->catfile('..', '..', 'file.txt');
+print "\n\n"                           ;
+
+print "curdir\n";
+print "------\n";
+
+print "\n" . File::Spec->curdir        ;
+print "\n\n"                           ;
+
+print "devnull\n";
+print "-------\n";
+
+print "\n" . File::Spec->devnull       ;
+print "\n\n"                           ;
+
+print "rootdir\n";
+print "-------\n";
+
+print "\n" . File::Spec->rootdir       ;
+print "\n\n"                           ;
+
+print "tmpdir\n";
+print "------\n";
+
+print "\n" . File::Spec->tmpdir        ;
+print "\n\n"                           ;
+
+print "updir\n";
+print "-----\n";
+
+print "\n" . File::Spec->updir         ;
+print "\n\n"                           ;
