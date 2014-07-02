be5d910309fe98954ffcc86e6c3990e8ca4bda1c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-02 10:42:28 +0200

    + GetShortPathName

--- Win32/script.pl
+++ Win32/script.pl
@@ -3,4 +3,11 @@ use strict;
 
 use Win32;
 
-print "OS Name: " . Win32 -> GetOSName() . "\n"; # for exampele «Win7»
+use File::Spec;
+use Cwd;
+
+my $path_of_script = File::Spec->canonpath(File::Spec->catdir(cwd, $0));
+
+print "OS Name:    " . Win32::GetOSName       ()                           . "\n"; # for exampele «Win7»
+print "Short Name: " . Win32::GetShortPathName($path_of_script)            . "\n";
+
