e300d30b4cf63bf9d6cfd37126d56df969520f4f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-03 22:53:49 +0200

    + Config

---
+++ Config/script.pl
@@ -0,0 +1,11 @@
+use warnings;
+use strict;
+
+use Config;
+
+printf "API Version:   %s\n", $Config{api_versionstring};
+printf "Arch Lib:      %s\n", $Config{archlib};
+printf "Byte Order:    %s\n", $Config{byteorder};
+printf "cat:           %s\n", $Config{cat};
+printf "ccname:        %s\n", $Config{ccname};
+printf "Hostname:      %s\n", $Config{aphostname};
