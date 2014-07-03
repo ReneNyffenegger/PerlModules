0925f0b8302db13bef1af11d822162e95a326bc1
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-03 22:33:21 +0200

    + POSIX

---
+++ POSIX/script.pl
@@ -0,0 +1,12 @@
+use warnings;
+use strict;
+
+use POSIX;
+
+printf "acos(     0) = %7.6f\n",     acos(     0);
+printf "asin(     1) = %7.6f\n",     asin(     1);
+printf "atan(100000) = %7.6f\n",     atan(100000);
+
+print "\n";
+
+print  "start of epoch: ", strftime ('%H:%M:%S %d.%m.%Y', gmtime(0)), "\n";
