642d03aaeefab7ac0fdd9201ee5f1208f0f62a38
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-07 14:10:56 +0200

    + Archive::Extract

---
+++ Archive/Extract/script.pl
@@ -0,0 +1,21 @@
+#
+#   Use this script to unzip a zip file.
+#   See also ../Zip/script.pl
+#
+use warnings;
+use strict;
+
+use Archive::Extract;
+use File::Spec::Functions 'tmpdir';
+
+die "Specify zip file" unless (@ARGV);
+
+my $zip_file = shift;
+
+my $temp_dir = tmpdir . '/unzipped-by-Archive-Extract';
+
+my $unzip = new Archive::Extract (archive => $zip_file);
+
+my $ok = $unzip->extract(to => $temp_dir);
+
+print "\nZipfile extracted to $temp_dir\n";
