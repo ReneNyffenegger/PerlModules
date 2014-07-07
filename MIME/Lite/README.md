8dfb443feae05e4739592824ac20895dc45d823e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-07 12:12:06 +0200

    + MIME::Lite

---
+++ MIME/Lite/README.md
@@ -0,0 +1,1 @@
+Note: MIME::Light is not recommended by its current maintainer (See [here](http://search.cpan.org/~rjbs/MIME-Lite-3.030/lib/MIME/Lite.pm#WAIT!)).
---
+++ MIME/Lite/script.pl
@@ -0,0 +1,37 @@
+#
+#   This script sends a mail with an attachment.
+#   Mutliple attachments can be sent by repeatedly
+#   calling $msg -> attach()
+#
+#   NOTE that MIME::Lite is not recommended by
+#   its current maintainer (7.7.2014).
+#
+use warnings;
+use strict;
+
+use MIME::Lite;
+
+my $sender_mail_address    = shift;
+my $smtp_server            = shift;
+my $recipient_mail_address = shift;
+my $subject                = shift;
+my $filename               = shift;
+my $auth_passwd            = shift;
+
+MIME::Lite->send('smtp', $smtp_server, Timeout=>60, AuthUser => $sender_mail_address, AuthPass => $auth_passwd);
+ 
+my $msg = new MIME::Lite(
+    From    => $sender_mail_address,
+    To      => $recipient_mail_address,
+    Subject => $subject,
+    Data    =>"Attached is a file.",
+    Type    =>"multipart/mixed",
+);
+ 
+$msg->attach('Type'     => 'application/octet-stream',
+             'Encoding' => 'base64',
+              Path      => $filename
+          );
+ 
+$msg->send();
+
