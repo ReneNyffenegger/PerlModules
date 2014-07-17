594fb4fe19d8e2e161d2eff474deae8ec6650d8f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-17 14:13:22 +0200

    select * from

--- DBD/CSV/script.pl
+++ DBD/CSV/script.pl
@@ -1,8 +1,8 @@
 use warnings;
 use strict;
-use feature 'say';
-
+use Lingua::EN::Numbers qw(num2en num2en_ordinal);
 use DBI;
+use feature 'say';
 
 
 
@@ -19,6 +19,25 @@ $dbh -> do ("create table csv_table (id integer, txt varchar(10), num real, blb 
 $dbh -> do ("insert into csv_table (id) values (1)");
 $dbh -> do ("insert into csv_table values (2, 'two', 22.2, 'twenty-two')");
 $dbh -> do ("update csv_table set txt = 'hello;world' num=42.2, blb='foo\nbar\nbaz' where id = 1");
+
+# -------
+
+my $sth = $dbh -> prepare ('insert into csv_table (id, txt, num, blb) values (?,?,?,?)');
+
+for (my $id=3; $id<100; $id++) {
+  $sth -> execute ($id, num2en($id), $id**2, num2en_ordinal($id));
+}
+
+# -------
+
+$sth = $dbh -> prepare ("select * from csv_table where id % 10 = ?");
+$sth -> execute(4); # Print records whose id end in 4.
+$sth -> bind_columns( \my ($id, $txt, $num, $blb) );
+
+while ($sth->fetch) {
+  printf "  %2i  %20s  %4i  %20s\n", $id, $txt, $num, $blb;
+}
+
 
 say "check $db_dir/csv_table and press a key";
 
