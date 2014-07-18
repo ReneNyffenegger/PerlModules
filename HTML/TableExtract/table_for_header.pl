58bb5d29a2b0a5e322f91b9c322d26e84a3c7ab6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-18 18:37:35 +0200

    + table_for_header*

---
+++ HTML/TableExtract/table_for_header.pl
@@ -0,0 +1,24 @@
+use warnings;
+use strict;
+
+use HTML::TableExtract;
+use String::Util 'trim';
+use File::Slurp;
+
+my $html = read_file('table_for_header.html');
+
+my $te = HTML::TableExtract -> new (
+   headers => ['Head 2', 'Head 3', 'Head 1'] # note: tr with «Head n» won't be displayed, col-2 will be printed after col-3.
+);
+
+$te -> parse($html);
+
+foreach my $table ( $te->tables ) {
+
+    print "\n\n\nTable (", join(',', $table->coords), "):\n-----------\n";   # (0,0)
+
+    foreach my $row ($table->rows) {
+      printf (("%-10s " x (@$row)) . "\n", map { $_ = trim $_; $_ ||= '-'; } @$row);
+    }
+
+}
