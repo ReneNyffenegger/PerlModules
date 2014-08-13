ca2be2c124e1543d3d793648730aa8d6c6568562
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-13 07:03:57 +0200

    Microsoft.Excel

--- Win32/OLE/Const/script.pl
+++ Win32/OLE/Const/script.pl
@@ -5,7 +5,12 @@ use Win32::OLE::Const;
 
 my $constants = Win32::OLE::Const->Load("Microsoft Office"); # or "Microsoft Office .* Object Library"
 
-
 foreach my $Key (sort keys %$constants) {
-    print "$Key = $constants->{$Key}\n";
+    printf "%-34s: %s\n", $Key, $constants->{$Key};
 }
+
+# ----
+
+use Win32::OLE::Const 'Microsoft.Excel';
+
+print "xlXYScatterSmoothNoMarkers = " , xlXYScatterSmoothNoMarkers, "\n";
