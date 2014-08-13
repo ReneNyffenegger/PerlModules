7e56c9aae13e2c34ebd695ef5c3bdb0048667399
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-13 07:35:58 +0200

    + Unicode::String

---
+++ Unicode/String/script.pl
@@ -0,0 +1,18 @@
+use warnings;
+use strict;
+use feature 'say';
+
+use Unicode::String qw(utf8 latin1 utf16le);
+use Data::Translate;
+
+my $d = new Data::Translate;
+
+my $x = "René";
+
+say length($x); # 5
+
+say $d -> a2h($x); # 152656ec3a9
+
+my $l1 = latin1($x);
+
+say $d -> a2h($l1);
