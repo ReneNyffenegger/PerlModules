8874414dfa3bf3b8b10b7e6486fdb759a3f676ad
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-21 12:24:58 +0200

    Is https not supported?

--- File/Fetch/script.pl
+++ File/Fetch/script.pl
@@ -3,6 +3,6 @@ use strict;
 
 use File::Fetch;
 
-my $fetcher = new File::Fetch (uri => 'https://raw.githubusercontent.com/ReneNyffenegger/PerlModules/master/File/Fetch/script.pl');
+my $fetcher = new File::Fetch (uri => 'http://www.adp-gmbh.ch/index.html');
 
 $fetcher->fetch;
