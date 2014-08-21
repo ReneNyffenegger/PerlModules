d077027df4d5af0441956026f13ad2e9f0104b50
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-21 15:47:30 +0200

    typo

--- LWP/UserAgent/script.pl
+++ LWP/UserAgent/script.pl
@@ -9,8 +9,8 @@ my $proxy_password = 'bar';
 # Setting agent to prevent «406 Not Acceptable» that some servers throw.
 my $ua = new LWP::UserAgent(agent => "The agent that never sleeps");
 
-# $ua -> proxy('http' , "http://$proxy_user:$proxy_password\@proxy.prod.zkb.ch:8080");
-# $ua -> proxy('https', "http://$proxy_user:$proxy_password\@proxy.prod.zkb.ch:8080");
+# $ua -> proxy('http' , "http://$proxy_user:$proxy_password\@proxy.server.foo:8080");
+# $ua -> proxy('https', "http://$proxy_user:$proxy_password\@proxy.server.foo:8080");
 
 # my $response = $ua -> request(new HTTP::Request(GET=>'http://www.adp-gmbh.ch/index.html'));
 my $response = $ua -> request(new HTTP::Request(GET=>'https://raw.githubusercontent.com/ReneNyffenegger/PerlModules/master/LWP/UserAgent/script.pl'));
