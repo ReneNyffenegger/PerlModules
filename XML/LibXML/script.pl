cf8079ffe5cc409b44a3de920486b49893a42c27
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-10-09 11:47:07 +0200

    + XML::LibXML

---
+++ XML/LibXML/script.pl
@@ -0,0 +1,36 @@
+use XML::LibXML;
+use strict;
+use warnings;
+my $parser = new XML::LibXML;
+
+my $xmlp= $parser -> parse_file("stackoverflow_17722520.xml");
+my $rootel = $xmlp -> getDocumentElement();
+
+my $elname = $rootel -> getName();
+my @rootelements=$rootel -> getAttributes();
+
+
+foreach my $rootatt(@rootelements){
+    my  $name = $rootatt -> getName();
+    my $value = $rootatt -> getValue();
+    print " ${name}[$value]\n ";
+}
+
+my @kids = $rootel -> childNodes();
+foreach my $child(@kids) {
+# print "\nchild: $child\n";
+  my @grand_kids = $child -> childNodes();
+
+  foreach my $grand_kid (@grand_kids) {
+
+    my $elname = $grand_kid -> getName();
+    my @atts = $grand_kid -> getAttributes();
+    foreach my $at (@atts) {
+        print "--\n";
+        my $name = $at -> getName();
+        my $value = $at -> getValue();
+        print "   ${name}[$value]\n ";
+  
+    }
+  }
+}
