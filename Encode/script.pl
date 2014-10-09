192aee0e720fdb420522b5bee859ae886e32a0f1
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-10-09 11:53:04 +0200

    From UTF-8 to Latin-1

--- Encode/script.pl
+++ Encode/script.pl
@@ -3,6 +3,14 @@ use strict;
 
 use Encode;
 
-print "Supported encodings:\n  ";
+print "From UTF-8 to Latin-1\n";
+my $auml = "\x{c3}\x{89}";
+printf("UTF-8  : %s\n", unpack('H*', $auml));
+Encode::from_to($auml, 'utf8', 'latin-1');
+printf("Latin-1: %s\n", unpack('H*', $auml));
+
+
+
+print "\n\nSupported encodings:\n  ";
 print join "\n  ", Encode->encodings(':all'); # supported encodings
 
