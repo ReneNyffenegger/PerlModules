f26c276150410d7a3e40d8aa66d5bd623ea95a87
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-10-27 17:54:19 +0100

    + Image::Magick::GetPixels.pl

---
+++ Image/Magick/GetPixels.pl
@@ -0,0 +1,22 @@
+use warnings;
+use strict;
+
+use Image::Magick;
+
+my $image = Image::Magick->new or die;
+my $read = $image -> Read('..\\..\\GD\\white-red-green-blue.png');
+
+for (my $x=0; $x<4; $x++) {
+
+  my @pixels = $image->GetPixels(
+      width     => 1,
+      height    => 1,
+      x         => $x,
+      y         => 0,
+      map       =>'RGB',
+      #normalize => 1
+  );
+  
+  printf "%3d %3d %3d\n", $pixels[0] / 256, $pixels[1] / 256, $pixels[2] / 256;
+
+}
