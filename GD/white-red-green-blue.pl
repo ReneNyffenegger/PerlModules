fc1bc2fec6b70f7f624744b7727ee72ca3b36146
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-10-27 20:53:12 +0100

    GD::trueColor(1)

--- GD/white-red-green-blue.pl
+++ GD/white-red-green-blue.pl
@@ -6,11 +6,13 @@ use strict;
 
 use GD;
 
+GD::Image -> trueColor(1);
+
 my $image = new GD::Image('white-red-green-blue.png') or die;
 
 for (my $x=0; $x<4; $x++) {
   my $index = $image->getPixel($x, 0);
   my ($r,$g,$b) = $image->rgb($index);
 
-  printf "%3d: %3d %3d %3d\n", $index, $r, $g, $b;
+  printf "%8d: %3d %3d %3d\n", $index, $r, $g, $b;
 }
