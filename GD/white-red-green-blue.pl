53697e64562360ed4a70a3896d493990cf190183
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-10-27 12:59:39 +0100

    + white-red-green-blue.png

---
+++ GD/white-red-green-blue.pl
@@ -0,0 +1,16 @@
+#
+#   http://stackoverflow.com/questions/26586918/how-do-i-get-the-correct-rgb-value-for-a-pixel-with-gd
+#
+use warnings;
+use strict;
+
+use GD;
+
+my $image = new GD::Image('white-red-green-blue.png') or die;
+
+for (my $x=0; $x<4; $x++) {
+  my $index = $image->getPixel($x, 0);
+  my ($r,$g,$b) = $image->rgb($index);
+
+  printf "%3d: %3d %3d %3d\n", $index, $r, $g, $b;
+}
