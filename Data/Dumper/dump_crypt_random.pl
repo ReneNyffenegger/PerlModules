db1b45c12d0070f6f7bd2fdac3226e641e847e2c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-10-31 06:49:44 +0100

    + dump_crypt_random.pl

---
+++ Data/Dumper/dump_crypt_random.pl
@@ -0,0 +1,21 @@
+#
+#  http://stackoverflow.com/a/5643298/180275
+
+use warnings;
+use strict;
+
+use Data::Dumper;
+
+use Crypt::Random qw(makerandom);
+
+print Dumper(
+#
+#     So let's look at what makerandom actually returns.
+
+      makerandom(Size => 256, Strength => 1)
+
+#     Ah, a Math::Pari object
+);
+
+# Same thing, on command line
+#   perl -MData::Dumper -MCrypt::Random=makerandom -e"print(Dumper(makerandom(Size => 256, Strength => 1)));"
