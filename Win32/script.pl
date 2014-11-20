3bd7f4a82f3c9dcd58e4b94e9cd2b0cfebd2a583
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-11-20 06:49:19 +0100

    Win32, more functions

--- Win32/script.pl
+++ Win32/script.pl
@@ -8,6 +8,22 @@ use Cwd;
 
 my $path_of_script = File::Spec->canonpath(File::Spec->catdir(cwd, $0));
 
-print "OS Name:    " . Win32::GetOSName       ()                           . "\n"; # for exampele «Win7»
-print "Short Name: " . Win32::GetShortPathName($path_of_script)            . "\n";
+print "Working dir: " . Win32::GetCwd          ()                           . "\n";
+print "OS Name:     " . Win32::GetOSName       ()                           . "\n"; # for exampele «Win7»
+print "Node Name    " . Win32::NodeName        ()                           . "\n"; 
+print "Short Name:  " . Win32::GetShortPathName($path_of_script)            . "\n";
+print "Login Name:  " . Win32::LoginName       ()                           . "\n";
 
+my ($fs, $flags, $maxpath) = Win32::FsType;
+
+print "Fs Type: $fs\n";
+print "  flags:   $flags\n";
+print "  maxpath: $maxpath\n";
+
+
+my ($sp, $major, $minor, $build, $id) = Win32::GetOSVersion;
+
+print "OS Version: $major.$minor\n";
+print "  build: $build\n";
+print "  id:    $id\n";
+print "  sp:    $sp\n";
