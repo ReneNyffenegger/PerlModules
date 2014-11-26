a00c782004559de8c6fe292f7ef87e4803262fe4
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-11-26 08:54:51 +0100

    + pairmap

--- List/Util/script.pl
+++ List/Util/script.pl
@@ -1,7 +1,8 @@
 use warnings;
 use strict;
 
-use List::Util qw(any min max reduce sum shuffle);
+use List::Util qw(any min max pairmap reduce sum shuffle);
+use List::MoreUtils;
 
 print "min: ", min (68, 38, 22, 42, 71, 57)  , "\n";
 print "max: ", max (68, 38, 22, 42, 71, 57)  , "\n";
@@ -26,3 +27,10 @@ print "foo exists in list\n"   if any {$_ eq 'foo'  } @words;
 print "seven exists in list\n" if any {$_ eq 'seven'} @words;
 # seven exists in list
 
+# ---
+
+my @a1 = qw(one   two     three four   );
+my @a2 = qw(apple oranges pears bananas);
+
+print join " - ", pairmap {"$a $b"} List::MoreUtils::mesh(@a1, @a2);
+# one apple - two oranges - three pears - four bananas
