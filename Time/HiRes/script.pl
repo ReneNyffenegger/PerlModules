924b14175d374dec43e277a3fc547a430c6c1e46
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-11-29 19:46:35 +0100

    time difference is in seconds

--- Time/HiRes/script.pl
+++ Time/HiRes/script.pl
@@ -10,4 +10,8 @@ print "bar\n";
 print "baz\n";
 my $end   = time;
 
-print $end-$start, "\n";
+print $end-$start, " seconds to print 3 times\n";
+
+"burn cpu for a second" while $end > time - 1;
+
+printf "%f second later\n", time - $end;
