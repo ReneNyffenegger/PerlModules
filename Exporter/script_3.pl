5d576bd3c7108d4088a8dbedeb82df3fab88144b
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-12-03 21:20:49 +0100

    Of course exp_one is now callable, too

--- Exporter/script_3.pl
+++ Exporter/script_3.pl
@@ -5,7 +5,7 @@ use feature 'say';
 
 use SomeModule (':all');
 
-say exp_one(); # Undefined subroutine &main::exp_one called at C:\github\PerlModules\Exporter\script_2.pl
+say exp_one();
 
 say ok_one();
 say ok_two();
