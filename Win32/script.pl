0c6c8bbedb6cb948266f60c55dfdb26aaa26388a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-12-03 08:22:49 +0100

    IsWin95 / IsWinNT

--- Win32/script.pl
+++ Win32/script.pl
@@ -10,6 +10,8 @@ my $path_of_script = File::Spec->canonpath(File::Spec->catdir(cwd, $0));
 
 print "Working dir: " . Win32::GetCwd          ()                           . "\n";
 print "OS Name:     " . Win32::GetOSName       ()                           . "\n"; # for exampele «Win7»
+print "  Win 95?    " . Win32::IsWin95         ()                           . "\n";
+print "  Win NT?    " . Win32::IsWinNT         ()                           . "\n";
 print "Node Name    " . Win32::NodeName        ()                           . "\n"; 
 print "Short Name:  " . Win32::GetShortPathName($path_of_script)            . "\n";
 print "Login Name:  " . Win32::LoginName       ()                           . "\n";
