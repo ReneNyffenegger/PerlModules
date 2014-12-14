a351ebf06b109da0cea2cb09927aa09c7db8cf4c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-12-14 07:44:08 +0100

    comment about cwd

--- Cwd/script.pl
+++ Cwd/script.pl
@@ -5,5 +5,5 @@ use feature 'say';
 use Cwd qw(abs_path cwd getcwd);
 
 say "abs_path: ", abs_path($0); # Get the full path to a Perl script that is executing / Compare with File::Spec, File::Spec->rel2abs
-say "cwd:      ", cwd;
+say "cwd:      ", cwd;          # Get path of current working directory (without trailing slashes)
 say "getcwd:   ", getcwd;       # compare with cwd
