efba22469ab8ea491e0a8b2c6836959e951881c8
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-12-14 07:57:28 +0100

    Using File::Basename / dirname so as to be able to call the script from 'anywhere'

--- Config/Std/script.pl
+++ Config/Std/script.pl
@@ -1,11 +1,12 @@
 use warnings;
 use strict;
+use feature 'say';
 
 use Config::Std;
 
-use feature 'say';
+use File::Basename;
 
-read_config 'script_config.ini' => my %cfg;
+read_config dirname($0) . '/script_config.ini' => my %cfg;
 
 say $cfg{'Section A'}{'key-1'     };
 say $cfg{'Section A'}{'equal-sign'};
