477a038d4c56b992c0c77acb6d6e668c9a6be304
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-06-27 09:15:18 +0200

    Use some parameters in 'new', call window.alert instead of alert

--- WWW/Mechanize/Firefox/script.pl
+++ WWW/Mechanize/Firefox/script.pl
@@ -3,13 +3,22 @@ use strict;
 
 use WWW::Mechanize::Firefox;
 
-my $mech = WWW::Mechanize::Firefox->new();
+my $mech = WWW::Mechanize::Firefox->new(
+  create    => 1,   # Create new tab...
+  activate  => 1,   # ... and activate it.
+  autoclose => 0,   # Keep tab open after perl script end.
+# --------------------------------------------------------
+# Name of the program if unable to connect on first try:
+  launch   =>'C:\Program Files\Mozilla Firefox\firefox.exe',
+);
+
 
 $mech->get('http://google.com');
 
 print "Click ok in firefox\n";
 
-$mech->eval_in_page('alert("Hello Firefox")');
+$mech->eval_in_page('window.alert("Hello Firefox")');
+# print $mech->eval_in_page('typeof(alert)');
 
 my $png = $mech->content_as_png();
 
