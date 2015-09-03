fdc7fa2f2682395363f7a20c4f3c034af4324c81
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-09-03 13:16:36 +0200

    + MP3::Info

---
+++ MP3/Info/script.pl
@@ -0,0 +1,26 @@
+use warnings;
+use strict;
+use MP3::Info;
+
+my $mp3_file = shift or die;
+die unless -f $mp3_file;
+
+my $mp3  = new MP3::Info ($mp3_file);
+
+foreach my $field qw(ARTIST ALBUM TITLE TRACKNUM YEAR GENRE COMMENT) {
+ printf("%-20s %s\n", ucfirst(lc $field), $mp3->{$field});
+}
+# Same thing?
+# -----------
+# my $tag  = get_mp3tag($mp3_file);
+# foreach my $field qw(ARTIST ALBUM TITLE TRACKNUM YEAR GENRE COMMENT) {
+#  printf("%-20s %s\n", ucfirst(lc $field), $tag->{$field});
+# }
+
+
+print "\n";
+ 
+my $t = get_mp3tag($mp3_file, 2, 1);
+print join "\n", keys %$t;
+
+print $mp3->{TPUB};
