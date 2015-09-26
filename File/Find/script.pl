5a375271614807dbc02c4ee3c71dc25663691763
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-09-26 21:29:28 +0200

    some comments

--- File/Find/script.pl
+++ File/Find/script.pl
@@ -13,9 +13,9 @@ find(\&file_find_callback, $root_dir);
 
 sub file_find_callback { # {{{
 
-    print "\$File::Find::dir   $File::Find::dir \n";
-    print "\$File::Find::name  $File::Find::name\n";
-    print "\$_                 $_\n";
+    print "\$File::Find::dir   $File::Find::dir \n"; # Absolute path to directory containing file
+    print "\$File::Find::name  $File::Find::name\n"; # Absolute path to file
+    print "\$_                 $_\n";                # Name of the file (without path information)
     print "relative path      ". File::Spec -> abs2rel($File::Find::name, $root_dir), "\n";
 
     print "\n";
