22b0a90ffa2a6f1ebedbd4b4618d44d3cbe78883
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-10-03 21:05:30 +0200

    + YAML::Tiny

---
+++ YAML/Tiny/script.pl
@@ -0,0 +1,34 @@
+use warnings;
+use strict;
+use feature 'say';
+use YAML::Tiny;
+
+my $yaml_file = 'data.yaml';
+
+unless (-e $yaml_file) {
+
+  my %data = (
+
+    'foo' => 42,
+    'bar' => 'baz',
+    'inner_hash' => {
+      one => 1,
+      two => 2
+    }
+  ); 
+
+  my $yaml = new YAML::Tiny (\%data);
+
+  $yaml->write($yaml_file);
+
+  exit 0;
+
+}
+
+my $yaml = YAML::Tiny->read($yaml_file);
+my %data = %{ $yaml->[0] };
+
+say $data{foo};
+say $data{bar};
+say $data{inner_hash}{one};
+say $data{inner_hash}{two};
