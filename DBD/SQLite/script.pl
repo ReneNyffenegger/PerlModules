ff46446c3e07a4ecf19bfe9236cfc6c36d5a34c6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-10-13 23:35:45 +0200

    + DBD::Sqlite

---
+++ DBD/SQLite/script.pl
@@ -0,0 +1,32 @@
+use warnings;
+use strict;
+
+use DBI;
+
+my $dbh = DBI->connect('dbi:SQLite:dbname=the.db') or die "Could not create the.db";
+
+$dbh -> do ('create table foo (col_1 number, col_2 varchar)');
+
+# Inserting values
+# ----------------
+
+my $sth = $dbh -> prepare('insert into foo values (:v1, :v2)');
+
+$sth -> execute(1, 'one');
+$sth -> execute(2, 'two');
+$sth -> execute(3, 'three');
+$sth -> execute(4, 'four');
+$sth -> execute(5, 'five');
+
+# Selecting values
+# ----------------
+
+$sth = $dbh -> prepare('select * from foo where col_1 > :v1 and col_2 like :v2');
+$sth -> execute(1, '%o%');
+
+while (my @r = $sth -> fetchrow_array) {
+  printf ("%2d %s\n", @r);
+}
+
+$dbh -> disconnect;
+
