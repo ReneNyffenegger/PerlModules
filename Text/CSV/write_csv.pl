b4132f4b055c57ff095148572bbafbcb2653c8c5
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-11-29 15:23:50 +0100

    Writing a CSV with Text::CSV

---
+++ Text/CSV/write_csv.pl
@@ -0,0 +1,12 @@
+use warnings;
+use strict;
+use utf8;
+
+use Text::CSV;
+
+open my $csv_fh, ">:encoding(utf8)", "written.csv" or die;
+
+my $csv = new Text::CSV( {binary=>1, eol=>"\n"}) or die;
+
+$csv -> print($csv_fh, ['foo', 'bar', 'baz']);
+$csv -> print($csv_fh, ['The foo, the bar and the baz', 'This is a quote:", is it not', 'Smiley: ☺']);
