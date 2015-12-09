b796fc50d35eead8506f44b4425f49a20fbb479b
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-12-09 12:11:33 +0100

    + event_handler.pl

---
+++ Win32/GUI/event_handler.pl
@@ -0,0 +1,18 @@
+use warnings;
+use strict;
+
+use Win32::GUI();
+
+my $wnd = new Win32::GUI::Window (-name=>'myWindow', -text=> 'the Window', -width => 300, -height => 200);
+$wnd -> AddLabel(-text => "Hello World");
+$wnd -> Show();
+
+Win32::GUI::Dialog();
+
+#
+#  A handler for the Terminate event.
+#
+sub myWindow_Terminate {
+  print "The window was terminated";
+  return -1; # returning 0 prevents the Window from being closed
+}
