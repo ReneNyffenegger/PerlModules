885354640f6ea1b358928e578344eddfe3885c75
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-12-09 12:46:30 +0100

    + SetWindowPos_HWND_TOPMOST.pl

---
+++ Win32/GUI/SetWindowsPos_HWND_TOPMOST.pl
@@ -0,0 +1,14 @@
+use warnings;
+use strict;
+
+use Win32::GUI;
+use Win32::GUI::Constants;
+
+my $HWND_TOPMOST = Win32::GUI::Constants::constant('HWND_TOPMOST');
+
+my $wnd = new Win32::GUI::Window (-text=> 'the Window');# , -width => 300, -height => 200);
+$wnd -> AddLabel(-text => "Hello World");
+$wnd -> SetWindowPos($HWND_TOPMOST, 300, 200, 100, 80, 0);
+$wnd -> Show();
+
+Win32::GUI::Dialog();
