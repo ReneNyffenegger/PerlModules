b38367fdad53ffbd660eb4212dc59cb30d246952
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-12-22 14:26:24 +0100

    + selector

---
+++ WWW/Mechanize/Firefox/selector.pl
@@ -0,0 +1,20 @@
+use warnings;
+use strict;
+use WWW::Mechanize::Firefox;
+
+my $mech = new WWW::Mechanize::Firefox (
+  launch   =>'C:\Program Files\Mozilla Firefox\firefox.exe',
+);
+
+$mech->get_local('selector.html');
+
+print "selecting #baz\n";
+
+my $baz = $mech->selector('#baz', single=>1);
+print "  ", $baz->{innerHTML}, "\n";
+
+print "selecting .foo_bar_baz\n";
+my @foo_bar_baz = $mech->selector('.foo_bar_baz', all=>1); 
+for my $foo_bar_baz (@foo_bar_baz) {
+  print "  ", $foo_bar_baz -> {innerHTML}, "\n";
+}
