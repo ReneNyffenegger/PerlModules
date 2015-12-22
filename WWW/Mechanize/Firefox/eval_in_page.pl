9908dac27eb9af857037b1f1bfe1732f6861acd8
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-12-22 14:02:19 +0100

    + eval_in_page

---
+++ WWW/Mechanize/Firefox/eval_in_page.pl
@@ -0,0 +1,30 @@
+use warnings;
+use strict;
+use WWW::Mechanize::Firefox;
+
+my $mech = new WWW::Mechanize::Firefox (
+  launch   =>'C:\Program Files\Mozilla Firefox\firefox.exe',
+);
+
+$mech->get_local('eval_in_page.html');
+
+my ($obj, $obj_type) = $mech->eval_in_page(<<'JS');
+
+  var e = document.getElementById('foo');
+
+//window.alert(e.innerHTML);
+//e.innerHTML;
+
+ var r = {
+   'text': e.innerHTML,
+   'xyz' : 42,
+  };
+
+ r;
+
+
+JS
+
+print "object type: $obj_type\n";
+print $obj->{text},"\n";
+print $obj->{xyz},"\n";
