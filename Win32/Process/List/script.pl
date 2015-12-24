957d76c080454033f54b475280c07520da7e816d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-12-24 06:54:45 +0100

    + Win32::Process::List

---
+++ Win32/Process/List/script.pl
@@ -0,0 +1,16 @@
+use warnings;
+use strict;
+use Win32::Process::List;
+
+my $procs = new Win32::Process::List;
+
+my $some_proc_name = 'explorer.exe';
+
+printf ("There are %d processes:\n", $procs->GetNProcesses());
+
+my %proc_list = $procs->GetProcesses();
+foreach my $key ( keys %proc_list ) {
+   print sprintf("%-30s  %5d\n", $proc_list{$key}, $key);
+}
+
+# printf ("%s has PID %s.\n", $some_proc_name, $procs->GetProcessPid($some_proc_name));
