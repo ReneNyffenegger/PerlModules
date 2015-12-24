1b8085abb04bad7296db744446b15b18e54c0b4a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-12-24 06:18:19 +0100

    + Win32::Process

---
+++ DBD/WMI/script.pl
@@ -0,0 +1,12 @@
+use warnings;
+use strict;
+use DBI;
+
+my $dbh = DBI->connect('dbi:WMI:');
+
+my $sth = $dbh->prepare('SELECT * FROM Win32_Process');
+$sth->execute();
+
+while (my $r = $sth->fetchrow) {
+  printf("%6d %-35s %-60s\n", $r->{ProcessId}, $r->{Caption}, $r->{ExecutablePath} || "<system>");
+}
