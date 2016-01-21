f4329d17c8908011c00d15ac9d5f1fa6753714c5
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-01-21 16:27:31 +0100

    + GD::Graph::bars

---
+++ GD/Graph/bars/script.pl
@@ -0,0 +1,27 @@
+use warnings;
+use strict;
+
+use GD::Graph::bars;
+use GD::Graph::Data;
+
+my $data = new GD::Graph::Data ([
+  [qw /Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec/],
+  [     10, 10, 12, 13, 16, 20, 23, 24, 22, 18, 13, 11 ], # Mean Temperature Barcelona
+]);
+
+my $graph = new GD::Graph::bars;
+
+$graph -> set (
+ 
+    x_label    => 'Month',
+    y_label    => 'Celsius',
+    title      => 'Mean temperature Barcelona'
+
+);
+
+$graph->plot($data);
+
+open my $png, '>', 'Temperature-Barcelona.png';
+binmode $png;
+print $png $graph->gd->png;
+close $png;
