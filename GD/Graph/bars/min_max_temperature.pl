716a9cb8cbf9ef7b013ee38f34e1507125ca5b05
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-01-21 17:17:59 +0100

    + min_max_temperature.pl

---
+++ GD/Graph/bars/min_max_temperature.pl
@@ -0,0 +1,32 @@
+use warnings;
+use strict;
+
+use GD::Graph::bars;
+use GD::Graph::Data;
+
+my $data = new GD::Graph::Data ([
+  [qw /Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec/],
+  [     14, 14, 16, 18, 22, 26, 28, 29, 26, 23, 18, 14 ], # Mean Max Temperature Barcelona
+  [      5,  6,  8, 10, 14, 18, 21, 21, 18, 14,  9,  5 ], # Mean MinjTemperature Barcelona
+]);
+
+my $graph = new GD::Graph::bars(500, 500);
+
+$graph -> set (
+ 
+    x_label     => 'Month',
+    y_label     => 'Celsius',
+    y_max_value =>  32,
+    title       => 'Mean temperature Barcelona',
+    transparent =>  0,
+    bar_spacing =>  8,
+    dclrs       => [qw/red blue/]
+
+);
+
+$graph->plot($data);
+
+open my $png, '>', 'Min-Max-Temperature-Barcelona.png';
+binmode $png;
+print $png $graph->gd->png;
+close $png;
