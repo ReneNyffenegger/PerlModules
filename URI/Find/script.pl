2d1a233073a07d4e3e031c36e7c8baf1e1be5b36
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-03-04 21:46:31 +0100

    + URI::Find

---
+++ URI/Find/script.pl
@@ -0,0 +1,33 @@
+use warnings;
+use strict;
+use URI::Find;
+
+my $linkifyer = new URI::Find (sub {
+    my $uri       = shift;
+    my $orig_uri  = shift;
+
+#   print "$uri  $orig_uri\n";
+
+    return "<a href='$uri'>$uri</a>";
+});
+
+open my $html, '>', 'script-out.html' or die;
+
+print $html "<html><body>";
+
+
+while (my $line = <DATA>) {
+
+  $linkifyer -> find(\$line);
+  print $html $line;
+
+}
+
+print $html "</body></html";
+close $html;
+
+
+__DATA__
+Here are a few links: http://www.renenyffenegger.ch, http://www.google.com, and http://www.github.com. Note 
+the trailing dot after github.org.
+
