15e0ee88f39e6ef158d2182db70f1e6073aad1b6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-03-23 17:49:13 +0100

    Download http instead of https page

--- LWP/Simple/script.pl
+++ LWP/Simple/script.pl
@@ -1,10 +1,19 @@
 use warnings;
 use strict;
-
 use LWP::Simple;
 
+my $url;
+
+# $url = 'https://raw.githubusercontent.com/ReneNyffenegger/PerlModules/master/LWP/Simple/script.pl';
+# This url prints
+#   500 write failed: Bad file descriptor
+# even if the PERL_LWP_SSL_VERIFY_HOSTNAME env variable
+# is set to 0.
+
+$url = 'http://www.google.com';
+
 # Just print a remote file
-getprint('https://raw.githubusercontent.com/ReneNyffenegger/PerlModules/master/LWP/Simple/script.pl');
+getprint($url);
 
 # Get the remote file and store it
-getstore('https://raw.githubusercontent.com/ReneNyffenegger/PerlModules/master/LWP/Simple/script.pl', 'script-downloaded.pl');
+getstore($url, 'google.html');
