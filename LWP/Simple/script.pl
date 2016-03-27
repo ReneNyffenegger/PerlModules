df57447ce2de2165dd53c0a404599f9ce1c71b82
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-03-27 07:40:20 +0200

    https works in Linux, though

--- LWP/Simple/script.pl
+++ LWP/Simple/script.pl
@@ -1,19 +1,19 @@
+#!/usr/bin/perl
 use warnings;
 use strict;
 use LWP::Simple;
 
 my $url;
 
-# $url = 'https://raw.githubusercontent.com/ReneNyffenegger/PerlModules/master/LWP/Simple/script.pl';
-# This url prints
+$url = 'https://raw.githubusercontent.com/ReneNyffenegger/PerlModules/master/LWP/Simple/script.pl';
+# On Windows, this url prints
 #   500 write failed: Bad file descriptor
 # even if the PERL_LWP_SSL_VERIFY_HOSTNAME env variable
 # is set to 0.
-
-$url = 'http://www.google.com';
+# $url = 'http://www.google.com';
 
 # Just print a remote file
 getprint($url);
 
 # Get the remote file and store it
-getstore($url, 'google.html');
+getstore($url, 'downloaded');
