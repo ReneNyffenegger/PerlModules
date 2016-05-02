56ae282c61c65bc35c15b7a6f3dbe9f0c1f45d54
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-05-02 15:17:33 +0200

    Add she-bang etc

--- HTML/Entities/script.pl
+++ HTML/Entities/script.pl
@@ -1,8 +1,10 @@
+#!/usr/bin/perl
 use warnings;
 use strict;
+use feature 'say';
 
 use utf8;
 
 use HTML::Entities;
 
-print encode_entities ('René');   # prints «Ren&eacute;»
+say encode_entities ('> René');   # prints «&gt; Ren&eacute;»
