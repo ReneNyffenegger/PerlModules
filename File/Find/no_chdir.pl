3b084b4375872a0f968c8c8c0218bf5b2727b507
René Nyffenegger <mail@renenyffenegger.ch>
2016-05-03 13:25:25 +0200

    + no_chdir.pl

---
+++ File/Find/no_chdir.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use File::Find;
+use File::Spec;
+
+my $root_dir = shift;
+
+
+find({ no_chdir => 1, 
+       wanted   => \&file_find_callback, },
+       $root_dir);
+    
+
+
+sub file_find_callback { # {{{
+
+    print "\$File::Find::dir   $File::Find::dir \n";
+    print "\$File::Find::name  $File::Find::name\n";
+    print "\$_                 $_\n";
+    print "relative path      ". File::Spec -> abs2rel($File::Find::name, $root_dir), "\n";
+
+    print "\n";
+    
+} # }}}
