1e34fceebb4ef82c2ac94b88427be4ab2569ad65
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-05-04 13:08:28 +0200

    + Encode::Guess

---
+++ Encode/Guess/script.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Encode::Guess; 
+
+my $data_latin1 = "Ren\xc9";
+my $data_utf8   = "Ren\x{c389}";
+
+print guess_encoding($data_latin1, 'latin1'), "\n";
+print guess_encoding($data_utf8  , 'latin1'), "\n";
