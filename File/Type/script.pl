59bd662b06716227e4b07471842069cc6d2f5790
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-10 14:03:03 +0100

    Unixify script.pl

--- File/Type/script.pl
+++ File/Type/script.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/perl
 use warnings;
 use strict;
 
@@ -8,7 +9,7 @@ use Cwd;
 
 my $ft = new File::Type;
 
-# Hmmm... Why do these, especially the .txt, all return «application/octet-stream»?
-print $ft -> mime_type ($0                                            ), "\n"; # application/octet-stream
+# Hmmm... Why does the .txt return «application/octet-stream»?
+print $ft -> mime_type ($0                                            ), "\n"; # application/x-perl
 print $ft -> mime_type (File::Spec->canonpath(cwd . '../../README.md')), "\n"; # application/octet-stream
 print $ft -> mime_type (File::Spec->canonpath(cwd . '/file.txt'      )), "\n"; # application/octet-stream
