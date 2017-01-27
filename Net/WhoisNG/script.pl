349a20426a8a319b6ff6e0f759efe7a95af46f1f
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-27 13:55:34 +0100

    + script.pl

---
+++ Net/WhoisNG/script.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+#   http://stackoverflow.com/a/11899899/180275
+#
+use warnings;
+use strict;
+
+use Net::WhoisNG;
+
+my $addr = shift or die;
+
+# my $whois = new Net::WhoisNG($addr) or die;
+
+$whois -> lookUp() or die;
+
+print $whois->getExpirationDate, "\n";
+
+foreach my $type (qw(admin tech registrant bill)) {
+
+  if (my $contact=$whois->getPerson($type)) {
+
+      print "$type\n";
+      my $email = $contact->getEmail();
+
+      if ($email and $email =~ /\S/) {
+          print "$email\n";
+      } else {
+          my $unparsed = join(' ', @{$contact->getCredentials()});
+        # Use an regexp to extract e-mail from freeform text here, you can even pick ready one somewhere here on SO
+          print "$unparsed\n";
+      }
+      print "----\n\n";
+  }
+
+}
