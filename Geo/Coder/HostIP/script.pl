735869595b674af0b5ffd1ce0246ea9483d6f328
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-27 13:06:35 +0100

    + script.pl

---
+++ Geo/Coder/HostIP/script.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use Geo::Coder::HostIP;
+
+my $ipnr = shift or die;
+
+# I can even call it menlo …
+my $menlo     = new Geo::Coder::HostIP; # default Server: www.hostip.info
+
+
+#  Get the coordinates.
+#  If successful, it also fills other values
+#  into $menlo, such as CountryCode etc, see below
+my @coordinates = $menlo->FetchIP($ipnr);
+
+if (@coordinates) {
+  printf "lat %s, lon %s\n", @coordinates;
+  printf "Country code: %s\n", $menlo->CountryCode;
+} else {
+    print "Nothing found for $ipnr\n";
+}
