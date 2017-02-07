a95e6cc195b3d160487a15b1bf0235367665af14
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-07 07:35:52 +0100

    + rectangles-and-circles.pl

---
+++ PDF/API2/rectangles-and-circles.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use PDF::API2;
+
+use constant mm => 25.4 / 72;
+
+my $pdf  = new PDF::API2;
+my $page = $pdf  -> page;
+my $gfx  = $page -> gfx;
+
+$gfx -> fillcolor('orange');
+$gfx -> rect(70/mm, 150/mm, 60/mm, 60/mm);
+$gfx -> fill;
+
+$gfx -> fillcolor('darkblue');
+$gfx -> circle( 70/mm, 150/mm, 20/mm);
+$gfx -> circle(130/mm, 210/mm, 20/mm);
+$gfx -> fill;
+
+$pdf -> saveas('rectangles-and-circles.pdf');
+$pdf -> end;
