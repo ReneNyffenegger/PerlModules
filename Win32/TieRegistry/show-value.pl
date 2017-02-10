19c96bb53ec87f59eb948f1f3582eb9db023ed37
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-10 10:32:41 +0100

    Rule

--- Win32/TieRegistry/show-value.pl
+++ Win32/TieRegistry/show-value.pl
@@ -3,8 +3,15 @@ use warnings;
 use strict;
 use feature 'say';
 
-use Win32::TieRegistry;
+use Win32::TieRegistry(Delimiter=>'/');
 
-my $path_value = $Registry->{CUser}->{Environment}->{PATH};
+my $path_value = $Registry->{'CUser/Environment//PATH'};
+
+#   Does rule «prepend value with delimiter» always have to
+#   be followed? It doesn't seem so:
+die unless $path_value eq $Registry->{'CUser/Environment/PATH'};
+
+#   Alternative way to get value:
+die unless $path_value eq $Registry->{CUser}->{Environment}->{PATH};
 
 say for (split ';', $path_value);
