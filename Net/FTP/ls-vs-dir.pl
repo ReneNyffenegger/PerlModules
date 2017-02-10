7d12d902fd2d3df9e67192c48d989a0a29491657
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-10 13:11:43 +0100

    + ls-vs-dir.pl

---
+++ Net/FTP/ls-vs-dir.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use Net::FTP;
+
+my $host     = shift or die;
+my $username = shift or die;
+my $password = shift or die;
+my $path     = shift or die;
+
+my $ftp = new Net::FTP($host) or die "Could not connect to $host";
+
+$ftp->login($username, $password) or die "Could not login as $username";
+$ftp->cwd($path);
+
+my @dir = $ftp->dir;
+my @ls  = $ftp->ls;
+
+print "dir:\n  ";
+print join "\n  ", @dir;
+
+print "\nls\n  ";
+print join "\n  ", @ls;
