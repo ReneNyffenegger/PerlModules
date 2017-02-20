12870b4b8d5bdc18868ef0b0691a7ed54e0ff80b
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-20 13:28:39 +0100

    dos -> unix

--- Geo/Coordinates/Transform/script.pl
+++ Geo/Coordinates/Transform/script.pl
@@ -1,12 +1,10 @@
+#!/usr/bin/perl
 use warnings; use strict;
 use Geo::Coordinates::Transform;
 
-
 my $g = Geo::Coordinates::Transform->new();
-
 
 printf "%s\n", @{$g->cnv_to_dms([ 20.5     ])}[0];   # 20 30 0.00000
 printf "%s\n", @{$g->cnv_to_ddm([ 20.5     ])}[0];   # 20 30.00000
 printf "%s\n", @{$g->cnv_to_dd (["20 30 0" ])}[0];   # 20.5000000
 printf "%s\n", @{$g->cnv_to_dd (["20 30 12"])}[0];   # 20.5033333
-
