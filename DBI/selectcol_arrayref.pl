c2b0365c0a19eefe2e32ee7dfaee45d5dc0a068a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-22 16:16:02 +0100

    selectcol_arrayref*

---
+++ DBI/selectcol_arrayref.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use DBI;
+
+unlink 'the.db' if -e 'the.db';
+my $dbh = DBI->connect('dbi:SQLite:dbname=the.db') or die "Could not create the.db";
+
+$dbh -> do('create table tab(col1, col2, col3)');
+
+my $sth = $dbh -> prepare('insert into tab values (?, ?, ?)');
+$sth -> execute('foo', 'one'  , 'abc');
+$sth -> execute('bar', 'two'  , 'def');
+$sth -> execute('baz', 'three', 'ghi');
+
+for (@{$dbh -> selectcol_arrayref('select col1 from tab')}) {
+  print "$_\n";
+}
+#
+# foo
+# bar
+# baz
