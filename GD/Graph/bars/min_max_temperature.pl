244acbba538411701dd8e94de2c3966504eae4c7
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-03 17:23:45 +0100

    775, she bang

--- GD/Graph/bars/min_max_temperature.pl
+++ GD/Graph/bars/min_max_temperature.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/perl
 use warnings;
 use strict;
 
@@ -7,7 +8,7 @@ use GD::Graph::Data;
 my $data = GD::Graph::Data->new([
   [qw /Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec/],
   [     14, 14, 16, 18, 22, 26, 28, 29, 26, 23, 18, 14 ], # Mean Max Temperature Barcelona
-  [      5,  6,  8, 10, 14, 18, 21, 21, 18, 14,  9,  5 ], # Mean MinjTemperature Barcelona
+  [      5,  6,  8, 10, 14, 18, 21, 21, 18, 14,  9,  5 ], # Mean Min Temperature Barcelona
 ]);
 
 my $graph = GD::Graph::bars->new(500, 500);
--- GD/Graph/bars/script.pl
+++ GD/Graph/bars/script.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/perl
 use warnings;
 use strict;
 
