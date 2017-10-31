9b10653130d207d85917a3e0ffbb65eaa63d30d8
René Nyffenegger <mail@renenyffenegger.ch>
2017-10-31 06:11:27 +0100

    + sqlite_create_function.count-calls.pl

---
+++ DBD/SQLite/sqlite_create_function.count-calls.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use 5.10.0;
+
+use DBI;
+
+my     $db = 'call_counter.db';
+unlink $db if -f $db;
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$db") or die "Could not create $db";
+
+$dbh->do('
+ create table tab (
+   abc text
+ )
+') or die;
+
+my $sth_ins = $dbh->prepare('insert into tab values (?)') or die;
+$sth_ins -> execute('foo');
+$sth_ins -> execute('foo');
+$sth_ins -> execute('bar');
+$sth_ins -> execute('baz');
+$sth_ins -> execute('baz');
+
+$dbh -> sqlite_create_function ('call_counter', 0, sub {
+  state $cnt = 0;
+
+  $cnt++;
+
+  return $cnt;
+
+}) or die;
+
+  my $sth_sel = $dbh->prepare('
+select
+  abc,
+  sum(call_counter()) sum_counter
+from
+  tab
+group by
+  abc') or die;
+
+  $sth_sel -> execute;
+
+while (my $rec = $sth_sel->fetchrow_hashref) {
+  printf "%s %d\n", $rec->{abc}, $rec->{sum_counter};
+}
